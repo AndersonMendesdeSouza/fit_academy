06b03eeb625ffbf1d2cc28e7908bd4d58d97e291
AndersonMendesdeSouza <andersonmendesdesouza2007@gmail.com>
2025-07-16 10:13:00 -0300

    Atualizando os dados com  que entram na Api com @

--- src/main/java/com/apiAcadmi/academi/dtos/dadosAtualizarAluno.java
+++ src/main/java/com/apiAcadmi/academi/dtos/dadosAtualizarAluno.java
@@ -2,17 +2,24 @@ package com.apiAcadmi.academi.dtos;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Email;
+
 import com.apiAcadmi.academi.sexo.Sexo;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record dadosAtualizarAluno(
 		@NotNull
 		Long id,
 
 		String nome,
+		@NotBlank @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}", message = "CPF deve estar no formato XXX.XXX.XXX-XX")
 		String cpf,
+	 @Email
 		String email,
+	      @NotBlank @Pattern(regexp = "\\d{2}\\ \\d{5}\\-\\d{4}")
 		String telefone,
 		Sexo sexo,
 		LocalDate datanascimento
--- src/main/java/com/apiAcadmi/academi/dtos/dadosCadastroAluno.java
+++ src/main/java/com/apiAcadmi/academi/dtos/dadosCadastroAluno.java
@@ -4,13 +4,24 @@ import java.time.LocalDate;
 
 import com.apiAcadmi.academi.sexo.Sexo;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record dadosCadastroAluno(
+		@NotBlank
 		String nome,
+		@NotBlank @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\.\\d{2}", message = "CPF deve estar no formato XXX.XXX.XXX-XX")
 		String cpf,
-		String email,
+		@NotBlank
+        @Email
+        String email,
+        @NotBlank @Pattern(regexp = "\\d{2}\\ \\d{5}\\-\\d{4}")
 		String telefone,
-	 
+	 @NotNull
 		LocalDate datanascimento,
+		 @NotNull
 		Sexo sexo		
 		) {
 
