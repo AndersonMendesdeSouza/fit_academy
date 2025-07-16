34f7e6d36a26b8280b5da732ef53a5cac384a979
AndersonMendesdeSouza <andersonmendesdesouza2007@gmail.com>
2025-07-16 11:14:57 -0300

    Update dadosAtualizarAluno.java

--- src/main/java/com/apiAcadmi/academi/dtos/dadosAtualizarAluno.java
+++ src/main/java/com/apiAcadmi/academi/dtos/dadosAtualizarAluno.java
@@ -2,11 +2,9 @@ package com.apiAcadmi.academi.dtos;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.Email;
-
 import com.apiAcadmi.academi.sexo.Sexo;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
@@ -15,13 +13,15 @@ public record dadosAtualizarAluno(
 		Long id,
 
 		String nome,
-		@NotBlank @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}", message = "CPF deve estar no formato XXX.XXX.XXX-XX")
+		 @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}", message = "CPF deve estar no formato XXX.XXX.XXX-XX")
 		String cpf,
 	 @Email
 		String email,
-	      @NotBlank @Pattern(regexp = "\\d{2}\\ \\d{5}\\-\\d{4}")
+	    @Pattern(regexp = "\\d{2}\\ \\d{5}\\-\\d{4}")
 		String telefone,
+		
 		Sexo sexo,
+		
 		LocalDate datanascimento
 
 ) {
